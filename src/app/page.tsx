ad385f0ee8797b094595351f3d73efb1abbe8377
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-07 08:37:07 -0300

    feat: add next-auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from 'next/image'
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next'
 
 export default function Home() {
+  const {data} = useSession()
+
   return (
     <div>
-      <h2>
-        Hello world!
-      </h2>
+      <div className='flex flex-col'>
+        <button onClick={() => signIn()}>Login</button>
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+      <h1>Olá {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ''} alt={`Imagem de perfil do ${data?.user?.name}`} />
     </div>
   )
 }
