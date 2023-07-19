6ce37853e023b559fc7ea952571c8f5b3ef0c383
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-19 09:29:16 -0300

    feat: add trip highlights component

--- src/app/trips/[tripId]/components/TripDescription.tsx
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -6,7 +6,7 @@ interface TripDescriptionProps {
 
 const TripDescription = ({ description }: TripDescriptionProps) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-5">
       <h2 className="font-semibold text-primaryDarker">Sobre a viagem</h2>
       <p className="text-xs leading-5 text-primaryDarker mt-4">{description}</p>
     </div>
