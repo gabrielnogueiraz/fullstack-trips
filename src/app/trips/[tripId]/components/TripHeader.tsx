25ed6899df862e9f12ecbbe78839a059c143fc21
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-19 00:33:20 -0300

    feat: add initial trip details and trip header

---
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Trip } from "@prisma/client";
+
+import Image from "next/image";
+import ReactCountryFlag from "react-country-flag";
+
+
+interface TriHeaderProps {
+  trip: Trip
+}
+
+const TripHeader = ({ trip }: TriHeaderProps) => {
+  return(
+    <div className="flex flex-col">
+       <div className="relative h-[280px] w-full">
+        <Image 
+          src={trip?.coverImage}
+          fill
+          style={{
+            objectFit: "cover"
+          }}
+          alt={trip.name}
+        />
+      </div>
+      <div className="flex flex-col p-5">
+        <h1 className="font-semibold text-xl text-primaryDarker">{trip.name}</h1>
+
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg/>
+          <p className='text-xs text-grayPrimary underline'>{trip.location}</p>
+        </div>
+
+        <p className="text-xs text-grayPrimary">
+          <span className="text-primary font-medium">R${trip.pricePerDay.toString()}</span> por dia
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TripHeader
--- src/components/RecommendedTrips/index.tsx
+++ src/components/RecommendedTrips/index.tsx
@@ -1,9 +1,16 @@
 import { Trip } from '@prisma/client'
 import React from 'react'
 import TripItem from '../TripItem'
+import { prisma } from '@/lib/prisma'
+
+async function getTrips() {
+  const trips = await prisma.trip.findMany({})
+
+  return trips
+}
 
 const RecommendedTrips = async () => {
-  const data = await fetch("http://localhost:3000/hello").then((res) => res.json())
+  const data = await getTrips()
 
   return(
     <div className='container mx-auto p-5'>
