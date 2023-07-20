e9e83685277fac4013fda9d3f0e5b4e46ebb0dde
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-20 14:10:30 -0300

    feat: add user reservations page

---
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -0,0 +1,69 @@
+import Button from "@/components/Button";
+import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import Image from "next/image";
+import ReactCountryFlag from "react-country-flag";
+
+
+interface UserReservationItemProps {
+  reservation: Prisma.TripReservationGetPayload<{
+    include: { trip: true };
+  }>;
+}
+
+const UserReservationItem = ({reservation}: UserReservationItemProps) => {
+  const { trip } = reservation
+
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+        <div className="flex item-center gap-3 pb-5 border-b border-grayLighter border-solid">
+          <div className="relative h-[106px] w-[124px]">
+            <Image 
+              className="rounded-lg"
+              src={trip.coverImage}
+              fill
+              style={{
+                objectFit: 'cover'
+              }}
+              alt={trip.name}
+            />
+          </div>
+
+          <div className="flex flex-col mt-5">
+            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+
+            <div className="flex items-center gap-1">
+              <ReactCountryFlag countryCode={trip.countryCode} svg/>
+              <p className='text-xs text-grayPrimary underline'>{trip.location}</p>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="flex-flex-col mt-5 text-primaryDarker">
+          <h3 className="text-sm">Data</h3>
+          <div className="flex item-center gap-1 mt-1">
+            <p className="text-sm">{format(new Date(reservation.startDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            {" - "}
+            <p className="text-sm">{format(new Date(reservation.endDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+          </div>
+
+          <h3 className="text-sm mt-5">Hóspedes</h3>
+          <p className="text-sm pb-5">{reservation.guests} hóspedes</p>
+
+          <h3 className="font-semibold text-primaryDarker mt-3 pt-5 border-t border-solid border-grayLighter">Informações sobre o preço</h3>
+
+          <div className="flex justify-between mt-1">
+            <p className="text-primaryDarker text-sm mt-2">Total:</p>
+            <p className="font-medium text-sm">R${Number(reservation.totalPaid)}</p>
+          </div>
+          <Button className="w-full mt-5" variant="danger">Cancelar</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserReservationItem;
--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,10 +1,18 @@
 'use client'
+
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import UserReservationItem from "./components/UserReservationItem";
+import { Prisma } from "@prisma/client";
 
 const MyTrips = () => {
-  const [reservations, setReservations] = useState()
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
+  
   const { status, data } = useSession()
 
   const router = useRouter()
@@ -15,7 +23,7 @@ const MyTrips = () => {
     }
 
     const fetchReservations = async () => {
-      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`)
+      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`)
       const json = await response.json()
 
       setReservations(json)
@@ -25,7 +33,12 @@ const MyTrips = () => {
   },[status])
 
   return (
-    <div>My Trips</div>
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-primaryDarker text-xl">Minhas viagens</h1>
+      {reservations?.map((reservation) => (
+        <UserReservationItem key={reservation.id} reservation={reservation}/>
+      ))}
+    </div>
   );
 }
 
