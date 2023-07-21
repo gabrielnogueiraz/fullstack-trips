14e6cfe8c1bd788e3bfc90a69eb8bc71559e9eda
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-21 11:07:18 -0300

    feat: add search functionality

--- src/components/QuickSearch/index.tsx
+++ src/components/QuickSearch/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const QuickSearch = () => {
-  return(
+  return (
     <div className="container mx-auto p-5">
       <div className="flex items-center">
         <div className="w-full h-[1px] bg-grayLighter"></div>
@@ -10,50 +11,37 @@ const QuickSearch = () => {
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className="flex w-full justify-between mt-5">
+      <div className="flex w-full justify-between mt-5 lg:mt-10 lg:justify-center gap-40">
         <div className="flex flex-col items-center gap-1">
-          <Image 
-            width={35}
-            height={35}
-            src="/hotel-icon.png"
-            alt="Hotel Icon"
-          />
-
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <Link href={`/trips/search?text=hotel`} className="flex flex-col items-center hover:text-primary transition-all">
+            <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
+            <p className="text-sm lg:text-base text-grayPrimary">Hotel</p>
+          </Link>
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <Image 
-            width={35}
-            height={35}
-            src="/farm-icon.png"
-            alt="Fazenda Icon"
-          />
 
-          <p className="text-sm text-grayPrimary">Fazenda</p>
+        <div className="flex flex-col items-center gap-1">
+          <Link href="/trips/search?text=fazenda" className="flex flex-col items-center hover:text-primary transition-all">
+            <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
+            <p className="text-sm lg:text-base text-grayPrimary">Fazenda</p>
+          </Link>
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <Image 
-            width={35}
-            height={35}
-            src="/cottage-icon.png"
-            alt="Chalé Icon"
-          />
 
-          <p className="text-sm text-grayPrimary">Chalé</p>
+        <div className="flex flex-col items-center gap-1">
+          <Link href="/trips/search?text=Chalé" className="flex flex-col items-center hover:text-primary transition-all">
+            <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
+            <p className="text-sm lg:text-base text-grayPrimary">Chalé</p>
+          </Link>
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <Image 
-            width={35}
-            height={35}
-            src="/inn-icon.png"
-            alt="Pousada Icon"
-          />
 
-          <p className="text-sm text-grayPrimary">Pousada</p>
+        <div className="flex flex-col items-center gap-1">
+          <Link href="/trips/search?text=pousada" className="flex flex-col items-center hover:text-primary transition-all">
+            <Image width={35} height={35} src="/inn-icon.png" alt="Pousada" />
+            <p className="text-sm lg:text-base text-grayPrimary">Pousada</p>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuickSearch
+export default QuickSearch;
--- src/components/TripSearch/index.tsx
+++ src/components/TripSearch/index.tsx
@@ -4,8 +4,23 @@ import Input from '../Input'
 import DatePicker from '../DatePicker'
 import CurrencyInput from '../CurrencyInput'
 import Button from '../Button'
+import { Controller, useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+
+interface TripSearchForm {
+  text: string,
+  startDate: Date | null,
+  budget: string
+}
 
 const TripSearch = () => {
+  const router = useRouter()
+  const { control, register, handleSubmit, formState: { errors } } = useForm<TripSearchForm>()
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
+  }
+
   return(
     <div className='container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat'>
       <h1 className='font-semibold text-2xl text-primaryDarker text-center'>
@@ -13,13 +28,43 @@ const TripSearch = () => {
       </h1>
 
       <div className='flex flex-col gap-4 mt-5'>
-        <Input placeholder='Onde você quer ir?'/> 
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
+
         <div className="flex gap-4">
-          <DatePicker placeholderText='Data de ida' onChange={() => {}} className='w-full'/>
-          <CurrencyInput placeholder='Orçamento'/>
+        <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker onChange={field.onChange} selected={field.value} placeholderText="Data Final" className="w-full" minDate={new Date()} />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+              />
+            )}
+          />
         </div>
 
-        <Button>Buscar</Button>
+        <Button onClick={() => {handleSubmit(onSubmit)()}}>Buscar</Button>
       </div>
     </div>
   )
