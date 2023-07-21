acfaeebfb90b1642fffe68ebc9e03f3ca57ddba8
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2023-07-21 09:10:28 -0300

    feat: add page reload and trip cancel

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -11,10 +11,11 @@ import { toast } from "react-toastify";
 interface UserReservationItemProps {
   reservation: Prisma.TripReservationGetPayload<{
     include: { trip: true };
-  }>;
+  }>
+  fetchReservations: () => void
 }
 
-const UserReservationItem = ({reservation}: UserReservationItemProps) => {
+const UserReservationItem = ({ reservation, fetchReservations }: UserReservationItemProps) => {
   const router = useRouter()
   const { trip } = reservation
 
@@ -29,7 +30,7 @@ const UserReservationItem = ({reservation}: UserReservationItemProps) => {
 
       toast.success("Reserva cancelada com sucesso!", { position: 'bottom-center' })
 
-      router.replace("/")
+      fetchReservations()
   }
 
   return (
